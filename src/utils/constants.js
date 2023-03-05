d3cd0131fd5d9b949d8534ae3c69b3e7948d3f6e
Anna-Gorgulenko <115218995+Anna-Gorgulenko@users.noreply.github.com>
2023-03-05 18:10:21 +0300

    Проектная работа 9.4

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,18 +1,34 @@
-//константы профиля
-export const nameProfile = document.querySelector(".profile__title")
-export const aboutProfile = document.querySelector(".profile__subtitle")
-export const avatarProfile = document.querySelector(".profile__avatar")
+export const initialCards = [
+   {
+       name: 'Архыз',
+       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+   },
+   {
+       name: 'Челябинская область',
+       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+   },
+   {
+       name: 'Иваново',
+       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+   },
+   {
+       name: 'Камчатка',
+       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+   },
+   {
+       name: 'Холмогорский район',
+       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+   },
+   {
+       name: 'Байкал',
+       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+   }
+];
 
-//кнопки открытия попапов
-export const profileUpdateAvatar = document.querySelector(".profile__edit-avatar")
-export const profileAddButton = document.querySelector(".profile__add-button")
-export const profileEditButton = document.querySelector(".profile__edit-button")
-
-//находим форму редактирования по ее name
-export const formEditProfile = document.forms.editForm
-
-//находим форму создания карточек по ее name
-export const formAddProfile = document.forms.addForm
-
-//находим форму обновления аватара по ее name
-export const formUpdateAvatar = document.forms.editAvatarForm
+export const formClassSelectors = {
+   inputSelector: '.popup__input',
+   submitButtonSelector: '.popup__save',
+   inactiveButtonClass: 'popup__save_disabled',
+   inputErrorClass: 'popup__input_type_error',
+   errorClass: 'error_visible'
+};
